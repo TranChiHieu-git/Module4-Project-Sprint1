31fbb0845d6b5571de51e8acd0a5c47c5e663a3a
wangtnq <wangtnq@gmail.com>
2020-07-09 21:51:52 +0700

    user page

--- front-end/src/app/user/user-routing.module.ts
+++ front-end/src/app/user/user-routing.module.ts
@@ -1,16 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {TestComponent} from '../user/test/test.component';
-import {AdminComponent} from '../admin/admin.component';
+import {RouterModule, Routes} from "@angular/router";
+import {TestComponent} from "../user/test/test.component";
+import {AdminComponent} from "../admin/admin.component";
+import {ListAdminComponent} from "../admin/list-admin/list-admin.component";
 
 const routes: Routes = [{
   path: 'user', component: AdminComponent,
   children: [
-    {path: 'test', component: TestComponent}
-  ],
-}
-];
+    {
+      path: 'test', component: TestComponent
+    },
+    // ví dụ
+    // {
+    //   path: 'bac', component: ListAdminComponent
+    // }
+  ]
+}];
 
 @NgModule({
   declarations: [],
