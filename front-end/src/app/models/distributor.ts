c5cd90bd76c2b2abb929cb591ab9ed5742d80b7a
ngocsonqni <sonnn.qni@gmail.com>
2020-07-09 21:54:53 +0700

    edit-delete-customermanagement

---
+++ front-end/src/app/models/distributor.ts
@@ -0,0 +1,8 @@
+export class Distributor {
+  id: number;
+  name: string;
+  address: string;
+  numberPhone: string;
+  email: string;
+  img: string;
+}
---
+++ front-end/src/app/user/user-routing.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {TestComponent} from '../user/test/test.component';
+import {AdminComponent} from '../admin/admin.component';
+
+const routes: Routes = [{
+  path: 'user', component: AdminComponent,
+  children: [
+    {
+      path: 'test', component: TestComponent
+    },
+  ]
+}];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ]
+})
+export class UserRoutingModule {
+}
