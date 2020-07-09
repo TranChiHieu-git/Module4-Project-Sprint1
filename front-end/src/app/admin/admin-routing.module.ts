d12190a6a8a08d3ba18e60455a283b6cc5654239
HieuNguyen <hieunguyen16491@gmail.com>
2020-07-09 10:20:24 +0700

    quan ly thuong hieu

--- front-end/src/app/admin/admin-routing.module.ts
+++ front-end/src/app/admin/admin-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
--- front-end/src/app/employee/employee-routing.module.ts
+++ front-end/src/app/employee/employee-routing.module.ts
@@ -7,6 +7,7 @@ import {WarehouseManagementComponent} from './warehouse-management/warehouse-man
 import {TestPartnerComponent} from './partner-management/test-partner/test-partner.component';
 import {TestWarehouseComponent} from './warehouse-management/test-warehouse/test-warehouse.component';
 import {HomeComponent} from './warehouse-management/home/home.component';
+import {BrandManagementComponent} from './warehouse-management/brand-management/brand-management.component';
 
 const routes: Routes = [{
   path: 'employee', component: EmployeeComponent,
@@ -19,7 +20,8 @@ const routes: Routes = [{
     {
       path: 'warehouse-management', component: WarehouseManagementComponent, children: [
         {path: 'test', component: TestWarehouseComponent},
-        {path: 'home', component: HomeComponent}
+        {path: 'home', component: HomeComponent},
+        {path: 'brand', component: BrandManagementComponent}
       ]
     }
   ]
