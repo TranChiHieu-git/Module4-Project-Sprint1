ec3e7a6217351cf777c51895fd74540d3fe46baf
Tran Chi Hieu <hieu57130154@gmail.com>
2020-07-10 08:32:47 +0700

    hieu update

--- front-end/src/app/user/user-routing.module.ts
+++ front-end/src/app/user/user-routing.module.ts
@@ -7,6 +7,11 @@ import {UserManageComponent} from './user-manage/user-manage.component';
 import {UserOdersComponent} from './user-oders/user-oders.component';
 import {UserOderDetailComponent} from './user-oder-detail/user-oder-detail.component';
 import {UserDetailComponent} from './user-detail/user-detail.component';
+import {HomeStoreComponent} from './home-store/home-store.component';
+import {HomeCakeComponent} from './home-store/home-cake/home-cake.component';
+import {HomeCandyComponent} from './home-store/home-candy/home-candy.component';
+import {DetailComponent} from './home-store/detail/detail.component';
+import {OrderButtonComponent} from './orderButton/orderButton.component';
 
 const routes: Routes = [
   {
@@ -29,7 +34,15 @@ const routes: Routes = [
   {
     path: '', component: UserComponent,
     children: [
-      {path: 'test', component: TestComponent}
+      {path: '', component: OrderButtonComponent}
+      ,
+      {
+        path: 'home-store', component: HomeStoreComponent, children: [
+          {path: 'cake', component: HomeCakeComponent},
+          {path: 'candy', component: HomeCandyComponent},
+          {path: 'detail', component: DetailComponent}
+        ]
+      }
     ],
   }
 ];
