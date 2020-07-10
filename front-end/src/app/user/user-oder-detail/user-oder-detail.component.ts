793d6970809085ddfa81563f0a62507a47d4d107
Tran Chi Hieu <hieu57130154@gmail.com>
2020-07-10 14:51:02 +0700

    update full

---
+++ front-end/src/app/user/user-oder-detail/user-oder-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-oder-detail',
+  templateUrl: './user-oder-detail.component.html',
+  styleUrls: ['./user-oder-detail.component.scss']
+})
+export class UserOderDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front-end/src/app/user/user.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {UserRoutingModule} from './user-routing.module';
+import {TestComponent} from './test/test.component';
+import {RouterModule} from '@angular/router';
+import {HomeRoutingModule} from './home-store/home-routing.module';
+import {HomeStoreModule} from './home-store/home-store.module';
+import {ShareModule} from '../shares/share.module';
+import {MaterialModule} from '../shares/material.module';
+import {UserForgetpasswordComponent} from './user-forgetpassword/user-forgetpassword.component';
+import {UserLoginComponent} from './user-login/user-login.component';
+import {UserRegisterComponent} from './user-register/user-register.component';
+import {OrderButtonComponent} from './orderButton/orderButton.component';
+import { UserManageComponent } from './user-manage/user-manage.component';
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  declarations: [TestComponent, UserManageComponent, OrderButtonComponent, UserRegisterComponent, UserLoginComponent, UserForgetpasswordComponent],
+  exports: [
+    OrderButtonComponent,
+    UserRegisterComponent,
+    UserLoginComponent,
+    UserForgetpasswordComponent,
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    RouterModule,
+    HomeRoutingModule,
+    HomeStoreModule,
+    ShareModule,
+    MaterialModule,
+  ]
+})
+export class UserModule {
+}
