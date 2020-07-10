e024247610f7b791bfcdffb9cb2f45e4e943b502
Tran Chi Hieu <hieu57130154@gmail.com>
2020-07-10 14:41:10 +0700

    reset

--- front-end/src/app/models/distributor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Distributor } from './distributor';
-
-describe('Distributor', () => {
-  it('should create an instance', () => {
-    expect(new Distributor()).toBeTruthy();
-  });
-});
--- front-end/src/app/models/distributor.ts
+++
@@ -1,8 +0,0 @@
-export class Distributor {
-  id: number;
-  name: string;
-  address: string;
-  numberPhone: string;
-  email: string;
-  img: string;
-}
