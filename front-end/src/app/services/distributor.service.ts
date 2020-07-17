c4e28918aa4e371656ec714be87bd26fd701d112
Tran Chi Hieu <hieu57130154@gmail.com>
2020-07-17 08:29:40 +0700

    hieu project ok

--- front-end/src/app/services/distributor.service.ts
+++ front-end/src/app/services/distributor.service.ts
@@ -8,25 +8,32 @@ import {Observable} from 'rxjs';
 })
 export class DistributorService {
   MY_API_URL = 'http://localhost:8080';
+  private httpOptions: any;
+
   constructor(private httpClient: HttpClient) {
   }
+
   findAll(): Observable<Distributor[]> {
-    return this.httpClient.get<Distributor[]>(this.MY_API_URL+"/distributor/list");
+    return this.httpClient.get<Distributor[]>(this.MY_API_URL + '/distributor/list');
   }
+
   findById(id: number): Observable<Distributor> {
     return this.httpClient.get<Distributor>(this.MY_API_URL + '/distributor/' + id);
   }
+
   create(distributor: Partial<Distributor>): Observable<Distributor> {
-    return this.httpClient.post<Distributor>(this.MY_API_URL+"/distributor/create", distributor);
+    return this.httpClient.post<Distributor>(this.MY_API_URL + '/distributor/create', distributor);
   }
+
   deleteById(id: number): Observable<void> {
     return this.httpClient.delete<void>(this.MY_API_URL + '/distributor/delete' + id);
   }
+
   editEmployee(distributor, distributorId): Observable<any> {
     return this.httpClient.put(this.MY_API_URL + '/' + distributorId, distributor);
   }
+
   // // Thach
-  T_API_URL = 'http://localhost:8080/';
 
   // findById(id: number): Observable<Distributor> {
   //   return this.httpClient.get<Distributor>(this.API_URL + '/distributor/' + id);
@@ -34,10 +41,13 @@ export class DistributorService {
   findByName(name: string): Observable<TypeOfDistributor> {
     return this.httpClient.get<TypeOfDistributor>(this.MY_API_URL + '/type_distributor/' + name);
   }
+
   save(item: Distributor): Observable<any> {
     return this.httpClient.post<any>(this.MY_API_URL + '/distributor', item);
   }
-  getAllDistributor(currentPage,size,search):Observable<any>{
-    return this.httpClient.get(this.MY_API_URL+ '/distributor/list'+ '?page=' +currentPage + '&size='+size+ '&search=' +search)
+
+  getAllDistributor(currentPage, size, search): Observable<any> {
+    return this.httpClient.get(this.MY_API_URL + '/distributor/list' + '?page=' + currentPage + '&size='
+      + size + '&search=' + search, this.httpOptions);
   }
 }
