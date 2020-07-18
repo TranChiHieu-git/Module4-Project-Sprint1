ef094ac3d52d3eb9f4a7be62bebdcf37940d4a0e
HieuNguyen <hieunguyen16491@gmail.com>
2020-07-18 15:45:10 +0700

    Local project was running properly

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -5,13 +5,21 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyB0KbDE_nvMQl0-EOCs34TCPNdIHXIVI2M",
-    authDomain: "brand-logo-ef5ce.firebaseapp.com",
-    databaseURL: "https://brand-logo-ef5ce.firebaseio.com",
-    projectId: "brand-logo-ef5ce",
-    storageBucket: "brand-logo-ef5ce.appspot.com",
-    messagingSenderId: "426111061370",
-    appId: "1:426111061370:web:7be0d68978654dfce19ac3"
+    // apiKey: "AIzaSyB0KbDE_nvMQl0-EOCs34TCPNdIHXIVI2M",
+    // authDomain: "brand-logo-ef5ce.firebaseapp.com",
+    // databaseURL: "https://brand-logo-ef5ce.firebaseio.com",
+    // projectId: "brand-logo-ef5ce",
+    // storageBucket: "brand-logo-ef5ce.appspot.com",
+    // messagingSenderId: "426111061370",
+    // appId: "1:426111061370:web:7be0d68978654dfce19ac3"
+    apiKey: 'AIzaSyAFbHzEL2J7oXY5bWTF6dA3DnO_iCj5W48',
+    authDomain: 'webapp-1b736.firebaseapp.com',
+    databaseURL: 'https://webapp-1b736.firebaseio.com',
+    projectId: 'webapp-1b736',
+    storageBucket: 'webapp-1b736.appspot.com',
+    messagingSenderId: '1077539336649',
+    appId: '1:1077539336649:web:e5fbf4e6a877218b887818',
+    measurementId: 'G-N3YS1JFN9K'
   }
 
 };
