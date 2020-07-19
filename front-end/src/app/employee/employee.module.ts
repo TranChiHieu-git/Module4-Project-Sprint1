4b01ef5acdbaedad36629762943a6133f4ff7cc4
hvduan97 <hvduan97@gmail.com>
2020-07-19 20:52:17 +0700

    hvd

--- front-end/src/app/employee/employee.module.ts
+++ front-end/src/app/employee/employee.module.ts
@@ -30,7 +30,7 @@ import {AngularFireModule} from '@angular/fire';
 import {AngularFireStorageModule} from '@angular/fire/storage';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
 import {environment} from '../../environments/environment';
-import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
     declarations: [PartnerManagementComponent, EmployeeDetailComponent, EmployeeManagerComponent,
@@ -57,7 +57,7 @@ import {MatPaginatorModule} from "@angular/material/paginator";
     NgxPaginationModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireStorageModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
     MatPaginatorModule,
   ],
   providers: [
