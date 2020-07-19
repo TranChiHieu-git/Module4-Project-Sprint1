3cffa7c1138e29ec19c84b3c4869d474a758f8b6
Tran Chi Hieu <hieu57130154@gmail.com>
2020-07-19 16:11:16 +0700

    project ok

--- front-end/src/app/admin/admin.component.ts
+++ front-end/src/app/admin/admin.component.ts
@@ -15,6 +15,7 @@ export class AdminComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
   search() {
     if (window.location.href === 'http://localhost:4200/admin/access-times') {
       this.route.navigate(['/admin/access-times', this.userName]);
