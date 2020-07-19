59dce67ce4b1e469ad2a1fa46010ff8e1e655cc7
haidakto321 <kieusonhai07@gmail.com>
2020-07-19 16:08:33 +0700

    hai

--- front-end/src/app/admin/admin-routing.module.ts
+++ front-end/src/app/admin/admin-routing.module.ts
@@ -4,6 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {AdminComponent} from './admin.component';
 import {ListAccountComponent} from './list-account/list-account.component';
 import {AccessTimesComponent} from './access-times/access-times.component';
+import {EmployeeDetailComponent} from '../employee/employee-manager/employee-detail/employee-detail.component';
 
 
 const routes: Routes = [
