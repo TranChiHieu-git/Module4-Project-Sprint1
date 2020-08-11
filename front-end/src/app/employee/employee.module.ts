0530a78b3b2f9d6b18030b038ae5601bbeed7a3a
C0220G1-LeMinhHuy <lehuy121@gmail.com>
2020-08-11 08:15:53 +0700

    huylm push code front-end search + create

--- front-end/src/app/employee/employee.module.ts
+++ front-end/src/app/employee/employee.module.ts
@@ -43,6 +43,7 @@ import {NgSelectModule} from '@ng-select/ng-select';
 import {FilterWareHousePipe} from './warehouse-management/bill/filter-ware-house.pipe';
 import {ToastrModule} from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [PartnerManagementComponent, EmployeeDetailComponent, EmployeeManagerComponent,
@@ -53,43 +54,44 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     WarehouseManagementComponent,
     PartnerManagementComponent
   ],
-  imports:
-[
-  CommonModule,
-  EmployeeRoutingModule,
-  RouterModule,
-  ReactiveFormsModule,
-  Ng2SearchPipeModule,
-  Ng2OrderModule,
-  HttpClientModule,
-  NgbModule,
-  MaterialModule,
-  MatDialogModule,
-  ShareModule,
-  MatCardModule,
-  NgxPaginationModule,
-  AngularFireStorageModule,
-  AngularFireDatabaseModule,
-  MatPaginatorModule,
-  AngularFireModule.initializeApp({
-    apiKey: 'AIzaSyAFbHzEL2J7oXY5bWTF6dA3DnO_iCj5W48',
-    authDomain: 'webapp-1b736.firebaseapp.com',
-    databaseURL: 'https://webapp-1b736.firebaseio.com',
-    projectId: 'webapp-1b736',
-    storageBucket: 'webapp-1b736.appspot.com',
-    messagingSenderId: '1077539336649',
-    appId: '1:1077539336649:web:e5fbf4e6a877218b887818',
-    measurementId: 'G-N3YS1JFN9K'
-  }),
-  AngularFirestoreModule,
-  AngularFireAuthModule,
-  FacebookModule.forRoot(),
-  MatProgressBarModule,
-  NgSelectModule
-],
+    imports:
+        [
+            CommonModule,
+            EmployeeRoutingModule,
+            RouterModule,
+            ReactiveFormsModule,
+            Ng2SearchPipeModule,
+            Ng2OrderModule,
+            HttpClientModule,
+            NgbModule,
+            MaterialModule,
+            MatDialogModule,
+            ShareModule,
+            MatCardModule,
+            NgxPaginationModule,
+            AngularFireStorageModule,
+            AngularFireDatabaseModule,
+            MatPaginatorModule,
+            AngularFireModule.initializeApp({
+                apiKey: 'AIzaSyAFbHzEL2J7oXY5bWTF6dA3DnO_iCj5W48',
+                authDomain: 'webapp-1b736.firebaseapp.com',
+                databaseURL: 'https://webapp-1b736.firebaseio.com',
+                projectId: 'webapp-1b736',
+                storageBucket: 'webapp-1b736.appspot.com',
+                messagingSenderId: '1077539336649',
+                appId: '1:1077539336649:web:e5fbf4e6a877218b887818',
+                measurementId: 'G-N3YS1JFN9K'
+            }),
+            AngularFirestoreModule,
+            AngularFireAuthModule,
+            FacebookModule.forRoot(),
+            MatProgressBarModule,
+            NgSelectModule,
+            BsDatepickerModule.forRoot()
+        ],
 providers: [
   {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
-  BrandService
+  BrandService,
 ]
 })
 
